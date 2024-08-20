620fde12bec1a0e3a37bd93b98a935d7b0dde9a2
rohat güneş <rohatgns44@gmail.com>
2024-08-20 17:00:48 +1000

    Add project files.

---
+++ drinkcoffe/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> coffeeList = new List<string>();
+
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.Write($"{i}. kahve ismini giriniz: ");
+            string coffeeName = Console.ReadLine();
+            coffeeList.Add(coffeeName);
+        }
+
+        Console.WriteLine("\nGirdiğiniz kahve isimleri:");
+        foreach (string coffee in coffeeList)
+        {
+            Console.WriteLine(coffee);
+        }
+    }
+}
+
+
+
